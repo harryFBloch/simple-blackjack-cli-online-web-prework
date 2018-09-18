a01998a5675e0e22c737a01593884a9e21e18e03
Harry Bloch <harryfbloch@gmail.com>
2018-09-18 19:37:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,10 +20,12 @@ end
 
 def get_user_input
   # code #get_user_input here
+  gets(move)
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
