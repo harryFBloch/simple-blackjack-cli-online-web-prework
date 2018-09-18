ac4bb0eadcc580d81d075a4d91645676c968e73d
Harry Bloch <harryfbloch@gmail.com>
2018-09-18 20:00:32 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,6 +66,7 @@ def runner
   # code runner here
   welcome()
   cardTotal = initial_round()
+  display_card_total(cardTotal)
   cardTotal = hit?(cardTotal)
   cardTotal < 21 ? hit?(cardTotal) : end_game(cardTotal)
 end
