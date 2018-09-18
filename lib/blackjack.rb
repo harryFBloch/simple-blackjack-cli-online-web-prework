bf69abd6734f9dbe3f667a067f06fbd91821b823
Harry Bloch <harryfbloch@gmail.com>
2018-09-18 19:40:37 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,8 @@ end
 def initial_round
   # code #initial_round here
   card1 = deal_card()
-  cardTotal = deal_card() + deal_card()
+  card2 = deal_card()
+  cardTotal = card1 + card2
   display_card_total(cardTotal)
   
 end
